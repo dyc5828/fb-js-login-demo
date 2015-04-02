42ce2744d9975c3fff2992d92d6323471aa1aa37
Dan Chen <dyc5828@gmail.com>
2015-04-02 13:36:34 -0700

    start

--- fb-auth.php
+++ fb-auth.php
@@ -16,31 +16,13 @@ $fb_js_helper = new FacebookJavaScriptLoginHelper();
 
 try {
     // fb session auth
-    $fb_session = $fb_js_helper->getSession();
-    // echo '<pre>'; var_dump($fb_session); echo "<hr/>";
 
     // fb graph api
-    $fb_request = new FacebookRequest($fb_session, 'GET', '/me');
-    $fb_response = $fb_request->execute();
-    $fb_profile = $fb_response->getGraphObject(GraphUser::className());
-    // echo '<pre>'; var_dump($fb_profile); echo "<hr/>";
-
-    // access fb profile
-    $fb_id = $fb_profile->getProperty('id');
 
     // db auth
     // check id and verififed
     // if id found, return user
     // if id not found, create user then return created
-    $db_reponse = [
-    	'id' => '832605520144931',
-    	'color' => 'blue',
-    ];
-    
-    if($fb_id == $db_reponse['id']) {
-    	$json = json_encode($db_reponse);
-    	echo $json;
-    }
 
 } catch (FacebookRequestException $ex) {
 	echo 'didnt get it';
--- fb.js
+++ fb.js
@@ -5,8 +5,6 @@ function statusChangeCallback(response) {
 	// check status
 	if (response.status === 'connected') {
 		// logged in on app
-		apiInfo();
-		fbAjaxCallback();
 	} else if (response.status === 'not_authorized') {
 		// not logged in on app
 	} else {
@@ -24,7 +22,6 @@ window.fbAsyncInit = function() {
 	FB.init({
 		appId	: '664654150308047',
 		xfbml	: true,
-		cookie	: true,
 		version	: 'v2.3'
 	});
 
@@ -35,24 +32,6 @@ window.fbAsyncInit = function() {
 
 function apiInfo() {
 	FB.api('/me', function(response) {
-	  // console.log(response);
-	  $('div#name').html(response.first_name);
-	});
-}
-
-function fbAjax() {
-	return $.ajax({
-		url: 'http://localhost:8080/itp/fb-js-login-demo/fb-auth.php',
-		type: 'GET',
-		dataType: 'JSON',
-	});
-}
-
-function fbAjaxCallback() {
-	var ajax = fbAjax();
-
-	ajax.done(function(response) {
-		// console.log('ajax reponse: ', response);
-		$('div#name').append('\'s favorite color is '+response.color+'!');
+	  console.log(response);
 	});
 }
