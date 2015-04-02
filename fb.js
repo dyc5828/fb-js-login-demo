8d0ea19e500332912834bf8dc7644e07e9633ff3
Dan Chen <dyc5828@gmail.com>
2015-04-02 13:20:24 -0700

    complete

--- fb.js
+++ fb.js
@@ -1,11 +1,12 @@
 function statusChangeCallback(response) {
-	console.log('statusChangeCallback');
-	console.log(response);
+	// console.log('statusChangeCallback');
+	// console.log(response);
 
 	// check status
 	if (response.status === 'connected') {
 		// logged in on app
-		myInfo();
+		apiInfo();
+		fbAjaxCallback();
 	} else if (response.status === 'not_authorized') {
 		// not logged in on app
 	} else {
@@ -32,8 +33,26 @@ window.fbAsyncInit = function() {
 	});
 };
 
-function myInfo() {
+function apiInfo() {
 	FB.api('/me', function(response) {
-	  console.log(response);
+	  // console.log(response);
+	  $('div#name').html(response.first_name);
+	});
+}
+
+function fbAjax() {
+	return $.ajax({
+		url: 'http://localhost:8080/itp/fb-js-login-demo/fb-auth.php',
+		type: 'GET',
+		dataType: 'JSON',
+	});
+}
+
+function fbAjaxCallback() {
+	var ajax = fbAjax();
+
+	ajax.done(function(response) {
+		// console.log('ajax reponse: ', response);
+		$('div#name').append('\'s favorite color is '+response.color+'!');
 	});
 }
