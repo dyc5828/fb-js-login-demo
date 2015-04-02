d810ff8dcea27516c33f002aa33f378367b4f7dc
Dan Chen <dyc5828@gmail.com>
2015-04-02 12:31:51 -0700

    initial commit

---
+++ fb.js
@@ -0,0 +1,39 @@
+function statusChangeCallback(response) {
+	console.log('statusChangeCallback');
+	console.log(response);
+
+	// check status
+	if (response.status === 'connected') {
+		// logged in on app
+		myInfo();
+	} else if (response.status === 'not_authorized') {
+		// not logged in on app
+	} else {
+		// not logged in on facebook
+	}
+}
+
+function checkLoginState() {
+	FB.getLoginStatus(function (response) {
+		statusChangeCallback(response);
+	});
+}
+
+window.fbAsyncInit = function() {
+	FB.init({
+		appId	: '664654150308047',
+		xfbml	: true,
+		cookie	: true,
+		version	: 'v2.3'
+	});
+
+	FB.getLoginStatus(function(response) {
+		statusChangeCallback(response);
+	});
+};
+
+function myInfo() {
+	FB.api('/me', function(response) {
+	  console.log(response);
+	});
+}
